de0c9d7a4293b959c455838a5586af3856a2eb11
Alex Noble and Dennise Ortega <dennise@>
2018-03-26 11:35:04 -0700

    add toggle to hide form and show list. Refactor form array

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,39 @@
+var blanks = ["#item1", "#item2","#item3"];
+
 var userText = [];
-var uppercaseArray =[];
+var upperCaseArray =[];
 
 
 
 $(document).ready(function() {
+  $("#thingsList").toggle();
   $("button#add").click(function(){
 
+    blanks.forEach(function(blank){
+      text=$(blank).val();
+      userText.push(text);
+    });
 
-    var text=$("#item1").val();
-    console.log(text);
-    userText.push(text);
-    var text2=$("#item2").val();
-    userText.push(text2);
-    var text3=$("#item3").val();
-    userText.push(text3);
+    // var text=$("#item1").val();
+    // userText.push(text);
+    // var text2=$("#item2").val();
+    // userText.push(text2);
+    // var text3=$("#item3").val();
+    // userText.push(text3);
     //$('ul').append('<li>' +userText.pop()+ '</li>');
     //$('ul').append('<li>' +userText.pop()+ '</li>');
 
-    var uppercaseArray=userText.map(function(word){
+    var upperCaseArray=userText.map(function(word){
       return word.toUpperCase();
     });
 
-    console.log(uppercaseArray.sort());
+    console.log(upperCaseArray.sort());
+    upperCaseArray.forEach(function(upperCaseWord){
+      $('ul').append('<li>' +upperCaseWord+ '</li>');
+    })
+
+    $("#form1").toggle();
+    $("#thingsList").toggle();
 
 
   });
